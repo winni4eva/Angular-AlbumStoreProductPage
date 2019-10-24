04ac7c1fdbfd6c4d6db2bbeb3b686c29a53a566e
winni4eva <adams4lyf@gmail.com>
2019-10-24 09:42:14 +0000

    en(tracks): added album interface

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -9,7 +9,7 @@ import { ProductService } from '../product.service';
 })
 export class ProductTracklistingComponent implements OnInit {
 
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) { }
 
